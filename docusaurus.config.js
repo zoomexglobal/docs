c2abba5e4be564c3b54c35b75d7e138c7b633506
gavin.lu <gavin.lu1@zoomex.com>
2023-07-21 11:35:42 +0800

    update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://zoomexglobal.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/doc-v3/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'zoomexglobal', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'doc-v3', // Usually your repo name.
 
   trailingSlash: false,
   onBrokenLinks: 'throw',
